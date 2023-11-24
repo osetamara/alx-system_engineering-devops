aeddd94c433faa82a3b04260f0e676f0c97ccfe1
osetamara <osemudiamebrifini@gmail.com>
2023-11-24 20:49:53 +0000

    killmenow

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 create a file in /tmp
 
-file { '/tmp/school]" :
-   content => "I love puppet",
-   mode  => '0744'
+file { '/tmp/school]' :
+   content => 'I love puppet',
+   mode  => '0744',
    owner => 'www-data',
    group => 'www-data',
 
+}
