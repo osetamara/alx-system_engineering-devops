faf967b44e657e6b6c00979bf77924d13886bf3a
osetamara <osemudiamebrifini@gmail.com>
2023-11-26 07:11:14 -0800

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 #the code will create a file name inside tmp/Directory
+
 file { '/tmp/school':
-   ensure  => 'file',
    content => 'I love puppet',
-   mode    => '07744',
+   mode    => '0744',
    Owner   => 'www-data',
    group   => 'www-data',
 }
