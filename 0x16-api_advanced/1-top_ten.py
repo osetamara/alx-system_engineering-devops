3f35df5fa25af21f541726cc15956b9fd5f2490f
osetamara <osemudiamebrifini@gmail.com>
2024-08-14 10:17:33 -0700

    top

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,18 +1,40 @@
 #!/usr/bin/python3
-"""Module for task 1"""
 
+"""
+This module contains a function to query the Reddit API and print the titles of the first 10 hot posts listed for a given subreddit.
+"""
+
+import requests
 
 def top_ten(subreddit):
-    """Queries the Reddit API and returns the top 10 hot posts
-    of the subreddit"""
-    import requests
+    """
+    This function queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
+    If an invalid subreddit is given, it prints None.
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
-        print('None')
+    Parameters:
+    subreddit (str): The name of the subreddit.
+    """
+    
+    # Set a custom User-Agent to avoid getting errors related to Too Many Requests
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    
+    # Construct the API URL for the given subreddit
+    url = f'https://oauth.reddit.com/r/{subreddit}/hot'
+    
+    # Send a GET request to the API
+    response = requests.get(url, headers=headers)
+    
+    # If the request is successful, the status code will be 200
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+        
+        # Get the list of posts
+        posts = data['data']['children']
+        
+        # Print the titles of the first 10 hot posts
+        for post in posts[:10]:
+            print(post['data']['title'])
     else:
-        [print(child.get("data").get("title"))
-         for child in sub_info.json().get("data").get("children")]
+        # If the request is not successful, print None
+        print(None)
